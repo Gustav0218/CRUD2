d7e8a89514b5a9d18cf442eddb9cdaa47524f3d4
Gustavo Ornelas <ornelasg93@gmail.com>
2022-09-27 17:05:21 -0500

    Se termina maquetado de CRUD

--- java.js
+++ java.js
@@ -2,3 +2,52 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msj");
 let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    console.log("button clicked");
+
+    formValidation();
+});
+
+let formValidation = () => {
+    if (input.value === "") {
+        msg.innerHTML = "El campo no puede estar vacio.";
+        console.log("failure");
+    } else {
+        console.log("successs");
+        msg.innerHTML = "";
+    }
+
+
+    //Guardando datos//
+
+    let data = {};
+    let acceptData = () => {
+        data["text"] = input.value;
+        console.log(data);
+        createPost();
+
+    };
+    if (input.value === "") {
+    } else {
+        acceptData();
+    };
+
+};
+
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+    `;
+    input.value = "";
+    return
+};
+
+
